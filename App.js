e65106d04225ef22fec85f422d71b7d4aa1abbaf
Ruchika Pokharna <ruchikapokharna11@gmail.com>
2023-11-02 17:26:47 +0530

    Episode-3 code Functional component & React Elemnt

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//React.createElement => ReactElement => JS Object => HTMLElement(render)
-// const heading = React.createElement("h1", "heading", "Namaste React");
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-//JSX => React.createElement => ReactElement => JS Object => HTMLElement(render)
-//JSX uses parcel which internally uses Babel for converting JSX to react
-const jsxHeading = <h1 id="jsxHeading">"Namaste React using JSX"</h1>
-root.render(jsxHeading)
+
+//Funtional Component
+const Title_1 = function (){
+    return (<h2><i>"Title as Java Script Normal Function"</i></h2>);
+}
+
+const Title_2 = () => {
+    return (<h2><i>"Title as Arrow Function"</i></h2>);
+}
+const Heading = () => (
+<div id = "container">
+    <Title_1/>
+    <Title_1></Title_1>
+    {Title_2()}
+    <h1 className="heading">"Namaste React using Functional Component"</h1>
+</div>);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Heading/>)
